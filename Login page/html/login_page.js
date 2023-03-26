9f4a0999501a3186371cc0866164f3c54fce9720
migeagle25 <migael.arendt@gmail.com>
2023-03-26 21:07:21 +0200

    Gewerk an Js file en html skoon gemaak

--- Login page/html/login_page.js
+++ Login page/html/login_page.js
@@ -1,37 +1,71 @@
-const myProgram = function () {
-    const myBox = document.getElementById(`my-form-box`);
-    myBox.addEventListener(`click`, function (evClick) {
-        try {
-            const ele = evClick.target;
-            const eleClass = ele.getAttribute('class');
+// const myProgram = function () {
+//     const myBox = document.getElementById(`my-form-box`);
+//     myBox.addEventListener(`click`, function (evClick) {
+//         try {
+//             const ele = evClick.target;
+//             const eleClass = ele.getAttribute('class');
             
-            switch (eleClass) {
-                case 'bleh':
-                    alert(`bleh geclick`);
-                    break;
-                case 'bleh1':
-                    alert('bleh 1 geclick');
-                    break;
-                default:
-                    alert('not hit');
-                    break;
-            }
-            console.log(eleClass);
-        } catch (error) {
-            console.dir(error);
-        }
-    });
-}
+//             switch (eleClass) {
+//                 case 'bleh':
+//                     alert(`bleh geclick`);
+//                     break;
+//                 case 'bleh1':
+//                     alert('bleh 1 geclick');
+//                     break;
+//                 default:
+//                     alert('not hit');
+//                     break;
+//             }
+//             console.log(eleClass);
+//         } catch (error) {
+//             console.dir(error);
+//         }
+//     });
+// }
 
 
 
-myProgram();
+// myProgram();
 
-window[`hetgeklik`] = function () {
-    alert("poeskop");
-}
+// window[`hetgeklik`] = function () {
+//     alert("poeskop");
+// }
 
 
-const login_page = function () {
+// const login_page = function () {
 
-}
+// }
+
+
+// const LoginChecking = function() {
+//     document.getElementById('email-input').value = sEmail;
+
+//     const FormBox = document.getElementById('form-box');
+//     FormBox.addEventListener('click', function(evClick)) {
+//         try{
+            
+//         }
+//     }
+    
+// }
+
+var
+sEmail = '';
+sPassword= '';
+
+const FormBox = document.getElementById('form-box');
+
+document.getElementById('email-input').value = sEmail;
+document.getElementById('password-input').value = sPassword;
+   
+    function btnClicked(){
+    console.log(sEmail);
+    }
+
+     FormBox.addEventListener('click', btnClicked);
+
+        
+        
+
+
+     
